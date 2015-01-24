b638fceec2d2f1bbac56ad3830206e94b3c4b974
yyunikov <yyunikov@gmail.com>
2015-01-24 23:56:39 +0200

    Added grunt, but removed vulcanization

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    jekyllConfig: grunt.file.readYAML('_config.yml'),
+
+    jekyll: {
+      build: {
+      },
+      serve: {
+        options: {
+          watch: true
+          //serve: true
+        }
+      }
+    },
+
+/*    vulcanize: {
+      options: {
+        strip: true,
+        csp: true,
+        inline: true
+      },
+      elements: {
+        options: {
+          excludes: {
+            imports: [
+              "polymer.html"
+            ]
+          }
+        },
+        files: {
+          'elements/app-layout.vulcanized.html': 'elements/app-layout.html'
+        }
+      }
+    }*/
+
+  });
+
+  // Plugin and grunt tasks.
+  require('load-grunt-tasks')(grunt);
+
+  // Task to run vulcanize and build the jekyll site
+  //grunt.registerTask('default', ['vulcanize', 'jekyll:build']);
+
+  grunt.registerTask('default', ['jekyll:build']);
+};
